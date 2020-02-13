1fa42bfb44bffe3f81f8256c8d0ad67f04946164
Bryant Omoregie <unclebryant@gmail.com>
2020-02-13 04:22:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  headphones = {:railroads => {:rent_in_dollars => {:black => 1, :red => 2, :yellow => 3, :pink => 4}}}
+  headphones = {:railroads => { :names => {}, :rent_in_dollars => {:black => 1, :red => 2, :yellow => 3, :pink => 4}}}
 end
 
 def monopoly_with_fourth_tier
